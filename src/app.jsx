05f5f402642479822563e84663a008d258bff26c
zabi-haz <hussein18to30@gmail.com>
2023-02-27 20:45:35 +0300

    css update

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,25 @@
 import "./style.css"
 import face from "./secondfaceedit.png";
+import emailjs from '@emailjs/browser';
+import {useRef}from "react"
+
+
 export const App = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_3f3805s', 'template_ipeue3o', form.current, 'ofUifwRblAs7N5Pyi')
+      .then((result) => {
+          console.log(result.text);
+          console.log("send ")
+      }, (error) => {
+          console.log(error.text);
+          console.log("not send")
+      });
+  };
+
   return(
     <div className="mian_container">
       <div className="input_container">
@@ -9,7 +28,7 @@ export const App = () => {
           <h2 className="title">
           </h2>
         </div>
-      <form action="" className="main_form">
+      <form ref={form} className="main_form" onSubmit={sendEmail}>
         <input type="text" placeholder="enter your name :" />
         <input type="text" placeholder="eneter your phone number : " />
         <input type="text" placeholder="eneter your school education : " />
